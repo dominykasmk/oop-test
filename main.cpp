ef0804090942ed5cf0e02fbd82d0010b05f17e00
Dominykas Makūnas <dominykas.makunas@mif.stud.vu.lt>
2022-02-09 18:43:11 +0200

    programa papraso ivesti varda pavarde ir tada tai isveda i std

--- main.cpp
+++ main.cpp
@@ -3,9 +3,18 @@
 
 using std::cout;
 using std::cin;
+using std::string;
+using std::endl;
 
 
 int main()
 {
+    cout << "Įveskite savo vardą ir pavardę: ";
+
+    string name;
+    getline(cin, name);
+
+
+    cout << name << endl;
     return 0;
 }
