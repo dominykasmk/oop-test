dec2baff8796f05e127a9a154e7f4cbef650e95f
Dominykas Makūnas <dominykas.makunas@mif.stud.vu.lt>
2022-02-09 19:24:20 +0200

    isvedama zinute modifikuota atitinkamai pagal varda, taip pasisveikinimas isspausdinamas remuose

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <string>
 
 using std::cout;
 using std::cin;
@@ -14,17 +14,36 @@ int main()
     string first_name;
     string last_name;
 
-    cin >> first_name >> last_name;
+    cin >> first_name;
 
+    string greeting;
+    first_name[0] = toupper(first_name[0]);
     if (first_name[first_name.size() - 1] == 's') {
+
         first_name[first_name.size() - 1] = 'i';
+        greeting = "Sveikas, " + first_name + "!";
+    }
+    else if (first_name[first_name.size() - 1] == 'a') {
+
+        greeting = "Sveika, " + first_name + "!";
+    }
+    else {
+
+        greeting = "Sveikas, " + first_name + "!";
     }
 
 
-    if (last_name[last_name.size() - 1] == 's') {
-        last_name[last_name.size() - 1] = 'i';
-    }
+    string border3 = "* " + greeting + " *";
 
-    cout << first_name << ", " << last_name << endl;
+    string border1(border3.size(), '*');
+    string border5(border1);
+
+    string temp(greeting.size() + 2, ' ');
+    string border2 = "*" + temp + "*";
+    string border4(border2);
+
+    cout << border1 << endl << border2 << endl << border3 << endl
+        << border4 << endl << border5 << endl;
+
     return 0;
 }
