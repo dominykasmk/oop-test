675b2d0ed71325e7778fa72ad1f23f846714d0c0
dominykasmk <dominykas.makunas@mif.stud.vu.lt>
2022-02-09 20:08:34 +0200

    pridėjau string validationo ir subuildintą programą

--- main.cpp
+++ main.cpp
@@ -9,12 +9,46 @@ using std::endl;
 
 int main()
 {
-    cout << "Įveskite savo vardą ir pavardę: ";
+    cout << "Įveskite savo vardą" << endl;
 
     string first_name;
     string last_name;
 
-    cin >> first_name;
+    // Patikrina ar ivestas stringas nera tuscias
+    do {
+
+        cout << "> ";
+        getline(cin, first_name);
+       
+        if (first_name.empty()) {
+            cout << "Nieko neįvedėte." << endl << endl;
+        }
+        else {
+            break;
+        }
+
+    } while (true);
+
+
+    // Patikrina ar ivestas stringas yra normalus zodis
+    do {
+
+        bool valid_string = true;
+        for (std::size_t i{}; i < first_name.length() && valid_string; i++) {
+            if (!(std::isalpha(first_name[i]) || std::isspace(first_name[i]))) {
+              valid_string = false; 
+            }
+        }
+
+        if (!valid_string) {
+            cout << "Neįvedėte taisyklingo vardo." << endl << endl;
+        }
+        else {
+            break;
+        }
+
+    } while (true);
+
 
     string greeting;
     first_name[0] = toupper(first_name[0]);
@@ -42,6 +76,7 @@ int main()
     string border2 = "*" + temp + "*";
     string border4(border2);
 
+    cout << endl << endl;
     cout << border1 << endl << border2 << endl << border3 << endl
         << border4 << endl << border5 << endl;
 
