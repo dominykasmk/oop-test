074ff560ea6ee1e52de3c57ae8bb0d8f87fd682c
Dominykas Makūnas <dominykas.makunas@mif.stud.vu.lt>
2022-02-09 18:53:51 +0200

    program isveda varda pavarde teisingu linksniu

--- main.cpp
+++ main.cpp
@@ -11,10 +11,20 @@ int main()
 {
     cout << "Įveskite savo vardą ir pavardę: ";
 
-    string name;
-    getline(cin, name);
+    string first_name;
+    string last_name;
+
+    cin >> first_name >> last_name;
+
+    if (first_name[first_name.size() - 1] == 's') {
+        first_name[first_name.size() - 1] = 'i';
+    }
 
 
-    cout << name << endl;
+    if (last_name[last_name.size() - 1] == 's') {
+        last_name[last_name.size() - 1] = 'i';
+    }
+
+    cout << first_name << ", " << last_name << endl;
     return 0;
 }
